92e28d008eeeb26cb04e7b41139a629182937437
Jake <jacobsolsky@gmail.com>
2022-09-20 22:00:17 +1000

    Updated pandas preprocessin to approximate mass and extract spectral type information. Added lookup table for spectral info

--- Database_preperation/database_transform.py
+++ Database_preperation/database_transform.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import re
+import math
 
 """
 Source: https://www.astronexus.com/hyg
@@ -17,7 +18,7 @@ lum = brightness of a star in comparison to the sun (at the point of the star)*
 dist = distance from earth/sun in parsecs (x=0, y=0, z=0) (ingore 10000000)
 spect = spectral type of the star (and binary info)*
 comp = Index of the star in the system (1, 2, 3 for a three star system)
-comp_base = if not empty, indicates a multiple star system, refers to database ID of the primary star --> convert ID to HD
+comp_primary = if not empty, indicates a multiple star system, refers to database ID of the primary star --> convert ID to HD
 
 X = x position of the star (sun at x=0)*
 Y = y position of the star (sun at y=0)*
@@ -36,25 +37,21 @@ filename = "Database_preperation/hygdata_v3.csv"
 
 stars_df = pd.read_csv(filename)
 stars_df = stars_df[stars_df["hd"].notnull()]
-stars_df[stars_df["Spectral Type"].isnull() & stars_df["lum"] < 0.2 ] = "Z0" # Way to code for brown dwarf
+#stars_df["spect"][stars_df["spect"].isnull() & stars_df["lum"] < 0.2 ] = "Z0" # Way to code for brown dwarf
 
-stars_df = stars_df[["hd", "hr", "gl", "bf", "proper", "lum", "spect", "comp", "comp_base", "x", "y", "z"]]
+stars_df = stars_df[["hd", "hr", "gl", "bf", "proper", "lum", "spect", "comp", "comp_primary", "x", "y", "z"]]
+stars_df["approx_mass"] = stars_df["lum"].pow(3.5)
+#print(stars_df["lum"].head(5))
+#print(stars_df['approx_mass'].head(5))
 
-print(list(stars_df["spect"]))
+print((stars_df["spect"].unique()))
 
-def findStars(spect):
-    # Find star using regex on spectral code
-    pattern  = ""
-    pass
+pattern1 = "([OBAFGKM][0-9]).?([IV]*)"
+#pattern_re = re.compile(pattern1)
 
-def findGalaxies(spect):
-    # Find galaxies using regex on spectral code
-    pattern = ""
-    pass
+#stars_df["star_flag"] = stars_df["spect"].apply(lambda x: findStars(x))
+stars_df["star_spectral_type"] = stars_df["spect"].str.extract("([OBAFGKM][0-9]).?")
+stars_df["star_HR_type"] =  stars_df["spect"].str.extract("[OBAFGKM][0-9].?([IV]*)")
+stars_df["mass_approx"] = stars_df["lum"].pow(3.5)
 
-def findNova(spect)
-    # Find Nova using regex on spectral code
-    pattern = ""
-    pass
-
-stars_df["star_flag"] = np.where(stars_df["spect"].apply(findStars))
+print(stars_df)
