8fe0d1a4a2aaa1b232292bbde661407f7e48f5a6
Hugh <hugh.hannan@student.uts.edu.au>
2022-10-17 00:02:59 +1100

    Added Blueprint gamemode to Unreal and set Player Pawn as player

--- Unreal_Laika/Source/Unreal_Laika/PlayerClass.h
+++ Unreal_Laika/Source/Unreal_Laika/PlayerClass.h
@@ -25,13 +25,6 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 	/// <summary>
 	/// Movement for player forwards or backwards
 	/// </summary>
@@ -42,4 +35,11 @@ public:
 	/// </summary>
 	/// <param name="Value"></param>
 	void MoveRight(float Value);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
